8a018129a92f6a575cc51deafc014a6fa1e268a6
Tiffany Seeber <tiffany@cashswipe.com>
2016-03-19 01:20:41 -0400

    Updating pop-up for fields

--- public/assignment/client/views/forms/fields.controller.js
+++ public/assignment/client/views/forms/fields.controller.js
@@ -9,56 +9,81 @@
     function FieldController($scope, FieldService, $routeParams) {
         $scope.addField = addField;
         $scope.removeField = removeField;
+        $scope.updateField = updateField;
 
-        var fieldTypes = [
-            { "Single Line Text Field" :
-                 {"_id": null, "label": "New Text Field", "type": "TEXT", "placeholder": "New Field"}},
-            { "Multi Line Text Field" :
-                {"_id": null, "label": "New Text Field", "type": "TEXTAREA", "placeholder": "New Field"}},
-            { "Date Field" :
-                {"_id": null, "label": "New Date Field", "type": "DATE"}},
-            { "Dropdown Field" :
-                {"_id": null, "label": "New Dropdown", "type": "OPTIONS", "options": [
-                    {"label": "Option 1", "value": "OPTION_1"},
-                    {"label": "Option 2", "value": "OPTION_2"},
-                    {"label": "Option 3", "value": "OPTION_3"}
-                ]}},
-            { "Checkboxes Field" :
-                {"_id": null, "label": "New Checkboxes", "type": "CHECKBOXES", "options": [
-                    {"label": "Option A", "value": "OPTION_A"},
-                    {"label": "Option B", "value": "OPTION_B"},
-                    {"label": "Option C", "value": "OPTION_C"}]} },
-            { "Radio Buttons Field" :
-                {"_id": null, "label": "New Radio Buttons", "type": "RADIOS", "options": [
+        var formId   = $routeParams.formId;
+        updateFieldsView(formId);
+
+        var newFields = [
+            {   type : "Single Line Text Field",
+                field: {"_id": null, "label": "New Text Field", "type": "TEXT", "placeholder": "New Field"}},
+            {   type : "Multi Line Text Field",
+                field: {"_id": null, "label": "New Text Field", "type": "TEXTAREA", "placeholder": "New Field"}},
+            {   type : "Date Field",
+                field: {"_id": null, "label": "New Date Field", "type": "DATE"}},
+            {   type : "Dropdown Field",
+                field: {"_id": null, "label": "New Dropdown", "type": "OPTIONS", "options": [
+                            {"label": "Option 1", "value": "OPTION_1"},
+                            {"label": "Option 2", "value": "OPTION_2"},
+                            {"label": "Option 3", "value": "OPTION_3"}
+                        ]}},
+            {   type : "Checkboxes Field",
+                field:{"_id": null, "label": "New Checkboxes", "type": "CHECKBOXES", "options": [
+                        {"label": "Option A", "value": "OPTION_A"},
+                        {"label": "Option B", "value": "OPTION_B"},
+                        {"label": "Option C", "value": "OPTION_C"}]} },
+            {   type :"Radio Buttons Field",
+                field:{"_id": null, "label": "New Radio Buttons", "type": "RADIOS", "options": [
                     {"label": "Option X", "value": "OPTION_X"},
                     {"label": "Option Y", "value": "OPTION_Y"},
                     {"label": "Option Z", "value": "OPTION_Z"}]} }];
 
-        var formId   = $routeParams.formId;
-        var fields = [];
+        function updateFieldsView(formId){
+            FieldService
+                .getFieldsForForm(formId)
+                .then(function(response){
+                    $scope.fields = response.data;
+                });
+        }
 
-        FieldService
-            .getFieldsForForm(formId)
-            .then(function(response){
-                fields = response.data;
-                $scope.fields = fields;
-                console.log(fields);
-            });
-
+        function updateField(field, updateField){
+            console.log(field._id, updateField);
+            FieldService
+                .updateField(formId, field._id, $scope.field)
+                .then(function(response){
+                    $scope.fields = updateFieldsView(formId);
+                })
+        }
         function addField(fieldType){
-            /**7.	Implement event handler addField(fieldType)
-             a.	Uses FieldService to add a new field to a form
-             b.	Updates the view with the list of fields
-            **/
+            var newField;
+            for(var f in newFields){
+                if(fieldType == newFields[f].type){
+                    newField = newFields[f].field;
+                }
+            }
+
+            if(newField){
+                FieldService
+                    .createFieldForForm(formId, newField)
+                    .then(function(response){
+                        newField = response.data;
+
+                        if(!newField){
+                            $scope.error = "Field was not created";
+                        }
+
+                        updateFieldsView(formId);
+                    });
+
+            }
         }
 
         function removeField(field){
-            /**
-             * 8.	Implement event handler deleteField(field)
-             a.	Uses the FieldService to remove the field from a form
-             b.	Updates the view with the list of fields
-
-             */
+            FieldService
+                .deleteFieldFromForm(formId, field._id)
+                .then(function(response){
+                    updateFieldsView(formId);
+                });
         }
     }
 })();
