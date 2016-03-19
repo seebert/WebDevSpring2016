a915d949091fa3b8d16fd9fda0aad8fddd92fd65
Tiffany Seeber <tiffany@cashswipe.com>
2016-03-18 23:45:05 -0400

    Adding libraries for server-side requests

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var cookieParser = require('cookie-parser');
 var app = express();
+
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
-app.listen(port, ipaddress);
 
+app.use(multer());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+require("./public/assignment/server/app.js")(app);
+app.listen(port, ipaddress);
