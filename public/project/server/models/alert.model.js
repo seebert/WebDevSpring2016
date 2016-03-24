6c0e32fd2f606077877cd5c06c2de25b31f0f72e
Tiffany Seeber <tiffany@cashswipe.com>
2016-03-23 21:45:58 -0400

    Implemeting alert api

--- public/project/server/models/alert.model.js
+++ public/project/server/models/alert.model.js
@@ -1,3 +1,76 @@
 /**
  * Created by Tiffanys on 3/23/16.
  */
+var mock = require("./alert.mock.json");
+module.exports = function(app, db) {
+    var api = {
+        createAlert: createAlert,
+        findAllAlerts: findAllAlerts,
+        findAlertById: findAlertById,
+        findAlertsByPayeeId: findAlertsByPayeeId,
+        updateAlert: updateAlert,
+        deleteAlertById : deleteAlertById
+    };
+
+    return api;
+
+
+    function createAlert(alert) {
+        var alert = {
+            eventId: alert.eventId,
+            expenseId: alert.expenseId,
+            payeeId: alert.payeeId,
+            payerId: alert.payerId,
+            _id : (new Date).getTime()
+        };
+        mock.alerts.push(alert);
+        return alert;
+    }
+
+    function findAllAlerts(){
+        return mock;
+    }
+
+    function findAlertById(id){
+        for (var a in mock) {
+            if (mock[a]._id == id) {
+                return mock[a];
+            }
+        }
+    }
+
+
+    function findAlertsByPayeeId(id){
+        var alerts = [];
+        for (var a in mock) {
+            if (mock[a].payeeId == id) {
+                alerts.push(mock[a]);
+            }
+        }
+
+        return alerts;
+    }
+
+    function updateAlert(alertId, alert) {
+        for (var a in mock) {
+            if (mock[a]._id === alertId) {
+                mock[a].eventId = alert.eventId;
+                mock[a].expenseId = alert.expenseId;
+                mock[a].payeeId = alert.payeeId;
+                mock[a].payerId = alert.payerId;
+                return callback(mock[a]);
+            }
+        }
+        return null;
+    }
+
+    function deleteAlertById(alertId) {
+        for (var a in mock) {
+            if (mock[a]._id === alertId) {
+                mock.splice(a, 1);
+            }
+        }
+        return mock;
+    }
+
+};
