fda4ca6c6c506d13bc6d92d1b9828498e135d960
Tiffany Seeber <tiffany@cashswipe.com>
2016-03-24 12:34:52 -0400

    Adding functions for alert service

--- public/project/server/services/alert.service.server.js
+++ public/project/server/services/alert.service.server.js
@@ -3,10 +3,61 @@
  */
 module.exports = function(app, alertModel) {
     app.post('/api/project/alert', createAlert);
-    app.get('/api/project/alert/', getAllAlerts);
+    app.get('/api/project/alert', getAlerts);
     app.get('/api/project/alert?alertId=:alertId', getAlertById);
     app.get('/api/project/alert?payeeId=:payeeId', getAlertsByPayeeId);
     app.put('/api/project/alert/:alertId', updateAlertById);
     app.delete('/api/project/alert/:alertId', deleteAlertById);
+
+    function createAlert(req, res){
+        console.log("Create alert");
+        alertModel.createAlert(req.body);
+        res.json(null);
+    }
+
+    function getAlerts(req, res){
+        if(req.query.alertId){
+            getAlertById(req,res);
+        }else if(req.query.payeeId) {
+            getAlertsByPayeeId(req,res);
+        }else{
+            getAllAlerts(req, res);
+        }
+    }
+
+    function getAlertById(req, res){
+        var alertId = req.query.alertId;
+        console.log("Get alert by id:" + alertId);
+        var alert = alertModel.findAlertById(alertId);
+
+        res.json(alert);
+    }
+
+    function getAlertsByPayeeId(req, res){
+        var payeeId = req.query.payeeId;
+        console.log("Get alert by payee id:" + payeeId);
+        var alert = alertModel.findAlertsByPayeeId(payeeId);
+
+        res.json(alert);
+    }
+
+    function getAllAlerts(req, res){
+        console.log("Get all alerts");
+        var alerts = alertModel.findAllAlerts();
+
+        res.json(alerts);
+    }
+
+    function updateAlertById(req, res){
+        alertModel.updateUser(req.params.alertId, req.params.alert);
+        var alerts = alertModel.findAllAlerts();
+        res.json(alerts);
+    }
+
+    function deleteAlertById(req, res){
+        alertModel.deleteAlertById(req.params.alertId);
+        var alerts = alertModel.findAllAlerts();
+        res.json(alerts);
+    }
 
 };
