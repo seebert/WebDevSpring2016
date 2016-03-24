a90ffdea97b3354f7a9fe6d11b6d4951f968db77
Tiffany Seeber <tiffany@cashswipe.com>
2016-03-24 19:47:24 -0400

    Declaring server models and services

---
+++ public/project/server/app.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+    var alertModel    = require("./models/alert.model.js")();
+    var eventModel   = require("./models/event.model.js")();
+    var expenseModel   = require("./models/expense.model.js")();
+    var paymentRequestModel   = require("./models/paymentRequest.model.js")();
+    var userModel   = require("./models/user.model.js")();
+
+    var alertService  = require("./services/alert.service.server.js")(app, alertModel);
+    var eventService  = require("./services/event.service.server.js")(app, eventModel);
+    var expenseService  = require("./services/expense.service.server.js")(app, expenseModel);
+    var paymentRequestService  = require("./services/paymentRequest.service.server.js")(app, paymentRequestModel);
+    var userService  = require("./services/user.service.server.js")(app, userModel);
+};
