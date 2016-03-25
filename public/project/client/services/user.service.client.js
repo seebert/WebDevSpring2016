dc676cd13eadedabbc9da5bf8c938e0c6323ce7f
Tiffany Seeber <tiffany@cashswipe.com>
2016-03-24 20:29:02 -0400

    Updating user service to pull data from api using http

--- public/project/client/services/user.service.client.js
+++ public/project/client/services/user.service.client.js
@@ -6,81 +6,47 @@
         .module("PaymentApp")
         .factory("UserService", UserService);
 
-    function UserService($rootScope) {
-        var users = [];
-        users = [
-            {	"_id":123, "firstName":"Alice",            "lastName":"Wonderland", "email" : "",
-                "username":"alice",  "password":"alice" },
-            {	"_id":234, "firstName":"Bob",              "lastName":"Hope", "email" : "",
-                "username":"bob",    "password":"bob"},
-            {	"_id":345, "firstName":"Charlie",          "lastName":"Brown", "email" : "",
-                "username":"charlie","password":"charlie"},
-            {	"_id":456, "firstName":"Dan",              "lastName":"Craig", "email" : "",
-                "username":"dan",    "password":"dan"},
-            {	"_id":567, "firstName":"John",           "lastName":"Doe", "email" : "",
-                "username":"sample",     "password":"sample"}
-        ];
-
-        var model = {
-            users: users,
+    function UserService($rootScope, $http) {
+        var api = {
             createUser: createUser,
             findAllUsers : findAllUsers,
             loginUser: loginUser,
+            findUserByUsername: findUserByUsername,
+            findUserById: findUserById,
             updateUser: updateUser,
             deleteUserById : deleteUserById,
             getCurrentUser : getCurrentUser,
             setCurrentUser : setCurrentUser
         };
-        return model;
+        return api;
 
-        function createUser(user, callback) {
-            var user = {
-                username: user.username,
-                password: user.password,
-                email: user.email,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                _id : (new Date).getTime()
-            };
-            model.users.push(user);
-            return callback(user);
+        function createUser(user) {
+            return $http.post("/api/assignment/user", user);
         }
 
-        function findAllUsers(callback){
-            return callback(model.users);
+        function findAllUsers(){
+            return $http.get("/api/project/user");
         }
 
-        function loginUser(username, password, callback) {
-            for (var u in model.users) {
-                if (model.users[u].username === username &&
-                    model.users[u].password === password) {
-                    return callback(model.users[u]);
-                }
-            }
-            return callback(null);
+        function loginUser(username, password) {
+            return $http.get('/api/project/user?username='+username+'&password='+password) ;
         }
 
-        function updateUser(userId, user, callback) {
-            for (var u in model.users) {
-                if (model.users[u]._id === userId) {
-                    model.users[u].firstName = user.firstName;
-                    model.users[u].lastName = user.lastName;
-                    model.users[u].username = user.username;
-                    model.users[u].password = user.password;
-                    model.users[u].email = user.email;
-                    return callback(model.users[u]);
-                }
-            }
-            return callback(null);
+        function findUserByUsername(username) {
+            return $http.get('/api/project/user?username='+username) ;
         }
 
-        function deleteUserById(userId, callback) {
-            for (var u in model.users) {
-                if (model.users[u]._id === userId) {
-                    model.users.splice(u, 1);
-                }
-            }
-            return callback(model.users);
+
+        function findUserById(id) {
+            return $http.get('/api/project/user/'+id);
+        }
+
+        function updateUser(userId, user) {
+            return $http.put('/api/project/user/'+userId, user);
+        }
+
+        function deleteUserById(userId) {
+            return $http.delete('/api/project/user/'+userId);
         }
 
         function setCurrentUser (user) {
