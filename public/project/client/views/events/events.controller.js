ffc43d18f2a07d9b2ec888e783d79fa5c4948368
Tiffany Seeber <tiffany@cashswipe.com>
2016-03-25 19:42:38 -0400

    Adding modal for adding new events

--- public/project/client/views/events/events.controller.js
+++ public/project/client/views/events/events.controller.js
@@ -4,7 +4,64 @@
 (function(){
     angular
         .module("PaymentApp")
-        .controller("EventsController", EventsController);
+        .controller("EventsController", EventsController)
+        .controller("EventListController", EventListController)
+        .controller("EventDetailsController", EventDetailsController);
+
+    function EventListController($scope, EventsService, UserService){
+        $scope.create = create;
+        var currentUser = UserService.getCurrentUser();
+        setScopeEvents();
+
+
+        function setScopeEvents(){
+            EventsService
+                .findEventsByAdminId(currentUser._id)
+                .then(function(response){
+                    $scope.events = response.data;
+                });
+        }
+
+        function create(newEvent){
+            newEvent.adminId = currentUser._id;
+            EventsService
+                .createEvent(newEvent)
+                .then(function(response){
+                    console.log("createEvent() > " + JSON.stringify(response.data));
+                    $scope.newEvent = null;
+                    setScopeEvents()
+                });
+        }
+
+    }
+
+    function EventDetailsController($scope, $routeParams, EventsService, ExpensesService){
+        var eventId = $routeParams.eventId;
+        console.log(eventId);
+        EventsService
+            .findEventsById(eventId)
+            .then(function(response){
+                $scope.event = response.data;
+
+                console.log($scope.event);
+                $scope.expenses = getExpensesById($scope.event.expenses);
+
+            });
+
+        function getExpensesById(expensesId){
+            var expenses = [];
+            for(e in  expensesId){
+                ExpensesService
+                    .findExpenseById(expensesId[e])
+                    .then(function(response){
+                        expenses.push(response.data);
+                    });
+            }
+
+            return expenses;
+
+        }
+    }
 
     function EventsController($scope, EventsService){
         $scope.events = EventsService.findAllEvents(render);
