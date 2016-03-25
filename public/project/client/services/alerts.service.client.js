5ac4b8f45528508f6c9dfc7db9ea02b4f4f34cd9
Tiffany Seeber <tiffany@cashswipe.com>
2016-03-24 21:10:09 -0400

    Updating alerts service to pull data from api using http

--- public/project/client/services/alerts.service.client.js
+++ public/project/client/services/alerts.service.client.js
@@ -7,16 +7,7 @@
         .factory("AlertsService", AlertsService);
 
     function AlertsService(){
-        var alerts = [];
-        alerts = [
-            { "_id" : 1, "eventId" : 1, "expenseId" : 1,
-                "payeeId" : 567, "payerId" : 123 },
-            { "_id" : 2, "eventId" : 1, "expenseId" : 2,
-                "payeeId" : 567, "payerId" : 234 }
-        ];
-
-        var model = {
-            alerts: alerts,
+        var api = {
             createAlert: createAlert,
             findAllAlerts: findAllAlerts,
             findAlertById: findAlertById,
@@ -25,64 +16,31 @@
             deleteAlertById : deleteAlertById
         };
 
-        return model;
+        return api;
 
-        function createAlert(alert, callback) {
-            var alert = {
-                eventId: alert.eventId,
-                expenseId: alert.expenseId,
-                payeeId: alert.payeeId,
-                payerId: alert.payerId,
-                _id : (new Date).getTime()
-            };
-            model.alerts.push(alert);
-            return callback(alert);
+        function createAlert(alert) {
+            return $http.post('/api/project/alert', alert);
         }
 
-        function findAllAlerts(callback){
-            return callback(model.alerts);
+        function findAllAlerts(){
+            return $http.get('/api/project/alert');
         }
 
-        function findAlertById(id, callback){
-            for (var a in model.alerts) {
-                if (model.alerts[a]._id == id) {
-                    return callback(model.alerts[a]);
-                }
-            }
+        function findAlertById(id){
+            return $http.get('/api/project/alert?alertId='+id);
         }
 
 
-        function findAlertsByPayeeId(id, callback){
-            var alerts = [];
-            for (var a in model.alerts) {
-                if (model.alerts[a].payeeId == id) {
-                   alerts.push(model.alerts[a]);
-                }
-            }
-
-            return callback(alerts);
+        function findAlertsByPayeeId(id){
+            return $http.get('/api/project/alert?payeeId='+id);
         }
 
-        function updateAlert(alertId, alert, callback) {
-            for (var a in model.alerts) {
-                if (model.alerts[a]._id === alertId) {
-                    model.alerts[a].eventId = alert.eventId;
-                    model.alerts[a].expenseId = alert.expenseId;
-                    model.alerts[a].payeeId = alert.payeeId;
-                    model.alerts[a].payerId = alert.payerId;
-                    return callback(model.alerts[a]);
-                }
-            }
-            return callback(null);
+        function updateAlert(alertId, alert) {
+            return $http.put('/api/project/alert/'+alertId, alert);
         }
 
-        function deleteAlertById(alertId, callback) {
-            for (var a in model.alerts) {
-                if (model.alerts[a]._id === alertId) {
-                    model.alerts.splice(a, 1);
-                }
-            }
-            return callback(model.alerts);
+        function deleteAlertById(alertId) {
+            return $http.delete('/api/project/alert/'+alertId);
         }
 
     }
