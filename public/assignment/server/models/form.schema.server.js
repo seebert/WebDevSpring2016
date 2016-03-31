ca1e091032664f0b5f14c46159cab960e021bae3
Tiffany Seeber <tiffany@cashswipe.com>
2016-03-31 01:04:09 -0400

    Minor updates to schemas

--- public/assignment/server/models/form.schema.server.js
+++ public/assignment/server/models/form.schema.server.js
@@ -1,3 +1,16 @@
 /**
  * Created by Tiffanys on 3/30/16.
  */
+module.exports = function (mongoose) {
+    var FieldSchema = require("./field.schema.server.js")();
+
+    // use mongoose to declare a user schema
+    var FormSchema = mongoose.Schema({
+        userId: String,
+        title: {type: String, default: "New Form"},
+        fields: [FieldSchema],
+        created: {type: Date, default : new Date()},
+        updated: {type: Date, default : new Date()}
+    }, {collection: 'form'});
+    return FormSchema;
+};
--- public/assignment/server/models/user.schema.server.js
+++ public/assignment/server/models/user.schema.server.js
@@ -1,3 +1,15 @@
 /**
  * Created by Tiffanys on 3/30/16.
  */
+module.exports = function (mongoose) {
+    // use mongoose to declare a user schema
+    var UserSchema = mongoose.Schema({
+        username: String,
+        password: String,
+        firstName: String,
+        lastName: String,
+        email: String,
+        phones: [String]
+    }, {collection: 'user'});
+    return UserSchema;
+};
