08438e162a3581ead534c4006164aac6c7ac2c64
Tiffany Seeber <tiffany@cashswipe.com>
2016-04-13 17:54:32 -0400

    Using user service to logout

--- public/assignment/client/views/header/header.controller.js
+++ public/assignment/client/views/header/header.controller.js
@@ -10,9 +10,16 @@
         $scope.$location = $location;
         $scope.logout = logout;
 
+
         function logout() {
-            UserService.setCurrentUser(null);
-            $location.url("/home");
+            UserService
+                .logout()
+                .then(function(){
+                    UserService.setCurrentUser(null);
+                    $location.url("/home");
+                });
+            //UserService.setCurrentUser(null);
+            //$location.url("/home");
         }
     }
 })();
