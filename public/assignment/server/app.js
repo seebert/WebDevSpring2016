cd536485d43ca9a5841a2e8bfa875adbcaa4fb75
Tiffany Seeber <tiffany@cashswipe.com>
2016-04-13 17:55:55 -0400

    Adding admin service

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -1,6 +1,7 @@
 module.exports = function(app, db, mongoose) {
     var userModel    = require("./models/user.model.js")(db, mongoose);
     var userService  = require("./services/user.service.server.js")(app, userModel);
+    var adminService  = require("./services/admin.service.server.js")(app, userModel);
 
     var formModel   = require("./models/form.model.js")(db, mongoose);
     var formService = require("./services/form.service.server.js")(app, formModel);
