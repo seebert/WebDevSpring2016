a0c61b910aab86c8c54e6f2f60591cffa2085449
Tiffany Seeber <tiffany@cashswipe.com>
2016-04-17 21:15:24 -0400

    Updating homepage to include edit/manage buttons

--- public/project/client/views/overview/overview.controller.js
+++ public/project/client/views/overview/overview.controller.js
@@ -8,7 +8,10 @@
 
     function OverviewController($scope, $location, EventsService, PaymentRequestsService, UserService){
         $scope.createEvent = createEvent;
+        $scope.updateEvent = updateEvent;
+        $scope.selectedEvent = selectedEvent;
         $scope.go = go;
+
         var currentUser = UserService.getCurrentUser();
         setScopeEvents();
 
@@ -18,13 +21,6 @@
                 $scope.alerts = response.data;
             });
 
-        /*AlertsService
-            .findAlertsByPayeeId(currentUser._id)
-            .then(function(response){
-                $scope.alerts = response.data;
-            });*/
-
-
         function setScopeEvents(){
             EventsService
                 .findEventsByAdminId(currentUser._id)
@@ -33,20 +29,40 @@
                 });
         }
 
+
+        function selectedEvent(event){
+
+            console.log("event:" + event);
+            $scope.selectedEvent = event;
+            $scope.updateEvent.title = event.title;
+            $scope.updateEvent.description = event.description;
+        }
+
         function createEvent(newEvent){
             newEvent.adminId = currentUser._id;
             EventsService
                 .createEvent(newEvent)
                 .then(function(response){
-                    console.log("createEvent() > " + JSON.stringify(response.data));
                     $scope.newEvent = null;
                     setScopeEvents()
                 });
         }
 
+        function updateEvent(origEvent, updateEvent){
+            origEvent.title = updateEvent.title;
+            origEvent.description = updateEvent.description;
+            console.log(origEvent);
+            EventsService
+                .updateEvent(origEvent._id, origEvent)
+                .then(function(response){
+                    $scope.updateEvent = null;
+                    setScopeEvents()
+                });
+        }
+
         function go( path ) {
-            console.log( path);
             $location.path( path );
         }
+
     }
 })();
